5dea0f62d9865a6980e407f3eddfb8683a61d2f9
Adrian Villar <avg333@hotmail.com>
2024-03-02 18:44:40 +0100

    Improved

--- broker/src/main/java/routing/BsRouter.java
+++ broker/src/main/java/routing/BsRouter.java
@@ -5,11 +5,15 @@ import domain.entities.Bs;
 import domain.entities.Ue;
 import java.util.Comparator;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 
-public record BsRouter(RoutingAlgorithmMode mode) {
+@RequiredArgsConstructor
+public class BsRouter {
 
-  // TODO: Implement use parallel streams to improve performance can be an option
+  private final RoutingAlgorithmMode mode;
+
   private static Bs distanceVector(final Ue ue, final List<Bs> bsList) {
+    // Implement use parallel streams to improve performance can be an option
     return bsList.stream()
         .min(Comparator.comparingDouble(bs2 -> getDistance(ue.getPosition(), bs2.getPosition())))
         .orElseThrow(() -> new IllegalArgumentException("No base stations available"));
@@ -18,9 +22,9 @@ public record BsRouter(RoutingAlgorithmMode mode) {
   private static double getDistance(final Position positionUe, final Position positionBs) {
     final double leg1 = positionUe.x() - positionBs.x();
     final double leg2 = positionUe.y() - positionBs.y();
-    return leg1 * leg1 + leg2 * leg2;
     // Technically, the distance is the square root of the sum of the squares of the legs
     // But we don't need to calculate the square root, because we only need to compare distances
+    return leg1 * leg1 + leg2 * leg2;
   }
 
   public Bs getBs(final Ue ue, final List<Bs> bsList) {
