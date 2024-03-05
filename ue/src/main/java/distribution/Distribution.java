7f29765cd3bcd5f9ae48073576b32fa94c62e60a
Adrian Villar <avg333@hotmail.com>
2024-03-05 02:00:41 +0100

    Improved

--- ue/src/main/java/distribution/Distribution.java
+++ ue/src/main/java/distribution/Distribution.java
@@ -21,7 +21,14 @@ public class Distribution {
     return switch (distributionMode) {
       case DETERMINISTIC -> param1;
       case UNIFORM -> rand.nextDouble() * (param1 - param2) + param2;
-      case EXPONENTIAL -> param1 != 0 ? Math.log(1 - rand.nextDouble()) / (-param1) : 0;
+      case EXPONENTIAL ->
+          param1 != 0
+              ? Math.log(1 - rand.nextDouble()) / (-param1)
+              : throwExceptionWhenDivisionByZero();
     };
+  }
+
+  private double throwExceptionWhenDivisionByZero() {
+    throw new IllegalArgumentException("param1 cannot be zero for exponential distribution");
   }
 }
--- ue/src/test/java/distribution/DistributionTest.java
+++ ue/src/test/java/distribution/DistributionTest.java
@@ -1,25 +1,27 @@
 package distribution;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.within;
 
 import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
 
-// TODO Improve tests
 class DistributionTest {
 
-  private static final int NUM_TESTS = 1000;
-  private static final double DELTA = 0.1;
+  private static final int NUM_TESTS = 10000000;
+  private static final double DELTA = 0.3;
 
   private Distribution distribution;
 
   @Test
   void shouldSetSeedSuccessfully() {
     final long seed = Instancio.create(Long.class);
-    final Distribution distribution1 = new Distribution(DistributionMode.UNIFORM, 0, 1);
+    final double param1 = Instancio.create(Double.class);
+    final double param2 = Instancio.create(Double.class);
+    final Distribution distribution1 = new Distribution(DistributionMode.UNIFORM, param1, param2);
     distribution1.setSeed(seed);
-    final Distribution distribution2 = new Distribution(DistributionMode.UNIFORM, 0, 1);
+    final Distribution distribution2 = new Distribution(DistributionMode.UNIFORM, param1, param2);
     distribution2.setSeed(seed);
     assertThat(distribution1.getRandom()).isEqualTo(distribution2.getRandom());
   }
@@ -27,24 +29,21 @@ class DistributionTest {
   @Test
   void getRandomReturnsParam1WhenDistributionModeIsDeterministic() {
     final double param1 = Instancio.create(Double.class);
-    final double param2 = Instancio.create(Double.class);
-    distribution = new Distribution(DistributionMode.DETERMINISTIC, param1, param2);
-
-    for (int i = 0; i < NUM_TESTS; i++) {
-      assertThat(distribution.getRandom()).isEqualTo(param1);
-    }
+    distribution =
+        new Distribution(DistributionMode.DETERMINISTIC, param1, Instancio.create(Double.class));
+    assertThat(distribution.getRandom()).isEqualTo(param1);
   }
 
   @Test
   void getRandomReturnsValueInRangeWhenDistributionModeIsUniform() {
-    final double param1 = 1.0;
-    final double param2 = 2.0;
+    final double param1 = Instancio.create(Double.class);
+    final double param2 = Instancio.create(Double.class);
     distribution = new Distribution(DistributionMode.UNIFORM, param1, param2);
 
     double sum = 0.0;
     for (int i = 0; i < NUM_TESTS; i++) {
-      double randomValue = distribution.getRandom();
-      assertThat(randomValue).isBetween(param1, param2);
+      final double randomValue = distribution.getRandom();
+      assertThat(randomValue).isBetween(Math.min(param1, param2), Math.max(param1, param2));
       sum += randomValue;
     }
     double average = sum / NUM_TESTS;
@@ -61,7 +60,7 @@ class DistributionTest {
 
     double sum = 0.0;
     for (int i = 0; i < NUM_TESTS; i++) {
-      double randomValue = distribution.getRandom();
+      final double randomValue = distribution.getRandom();
       assertThat(randomValue).isNotNegative();
       sum += randomValue;
     }
@@ -72,13 +71,11 @@ class DistributionTest {
   }
 
   @Test
-  void getRandomReturnsZeroWhenDistributionModeIsExponentialAndParam1IsZero() {
-    final double param1 = 0.0;
-    final double param2 = Instancio.create(Double.class);
-    distribution = new Distribution(DistributionMode.EXPONENTIAL, param1, param2);
-
-    for (int i = 0; i < NUM_TESTS; i++) {
-      assertThat(distribution.getRandom()).isZero();
-    }
+  void shouldThrowIllegalArgumentExceptionWhenParam1IsZeroForExponentialDistribution() {
+    distribution =
+        new Distribution(DistributionMode.EXPONENTIAL, 0.0, Instancio.create(Double.class));
+    assertThatThrownBy(distribution::getRandom)
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("param1 cannot be zero for exponential distribution");
   }
 }
